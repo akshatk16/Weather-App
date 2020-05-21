118dd4ffafcfc03e5f27da912af0ad78dc975523
Akshat Kumar Agarwal <akshat.16@outlook.com>
2020-05-21 17:16:37 +0530

    Updates

---
+++ precache-manifest.2535e2a8fb9bae6ae0d1436f21403f80.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdb5c83c31448bb79e762e334e7c4e7e",
+    "url": "/weather-app/index.html"
+  },
+  {
+    "revision": "f790026f89de0c969427",
+    "url": "/weather-app/static/css/main.f2f8ab9f.chunk.css"
+  },
+  {
+    "revision": "6c7b53a2e296d568c93e",
+    "url": "/weather-app/static/js/2.26aa75cc.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/weather-app/static/js/2.26aa75cc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f790026f89de0c969427",
+    "url": "/weather-app/static/js/main.7a6e98cb.chunk.js"
+  },
+  {
+    "revision": "12ef64cf8e1aad5a2dd2",
+    "url": "/weather-app/static/js/runtime-main.c90f5b59.js"
+  },
+  {
+    "revision": "701c10bf0287cba34103f01cf8d29877",
+    "url": "/weather-app/static/media/bg.701c10bf.jpg"
+  },
+  {
+    "revision": "c97cb84b74a52edcb496295687e87822",
+    "url": "/weather-app/static/media/cold-bg.c97cb84b.jpg"
+  },
+  {
+    "revision": "635ed87d2cc826fb213057ac40dfaff1",
+    "url": "/weather-app/static/media/warm-bg.635ed87d.jpg"
+  }
+]);
